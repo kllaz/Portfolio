5a63d8f748a3b6ca4614e19616dc455fec793597
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-11-01 16:27:48 +0300

    Add files via upload

---
+++ js/biggerImage.js
@@ -0,0 +1,8 @@
+/* making image bigger */
+function biggerImage(){
+    document.getElementById('image-of-K_Laz').style.width = '300px';
+}
+/* making it small again */
+function smallerImage() {
+    document.getElementById('image-of-K_Laz').style.width = '100px'
+}
