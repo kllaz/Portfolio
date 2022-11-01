df140ff34fd50920a5c074dc702174d7acd130d2
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-11-01 16:28:05 +0300

    Update biggerImage.js

--- js/biggerImage.js
+++ js/biggerImage.js
@@ -1,6 +1,6 @@
 /* making image bigger */
 function biggerImage(){
-    document.getElementById('image-of-K_Laz').style.width = '300px';
+    document.getElementById('image-of-K_Laz').style.width = '200px';
 }
 /* making it small again */
 function smallerImage() {
