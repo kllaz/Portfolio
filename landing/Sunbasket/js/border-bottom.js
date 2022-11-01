7e2cd153c2f6b9d94856fff2bfc194f50a05f958
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-11-01 13:05:02 +0300

    Add files via upload

---
+++ landing/Sunbasket/js/border-bottom.js
@@ -0,0 +1,26 @@
+/* border-bottom disappear */
+function changeFirstItem() {
+    document.getElementById('border-bottom-first').style.borderBottom = '0px solid black';
+}
+function changeSecondItem() {
+    document.getElementById('border-bottom-second').style.borderBottom = '0px solid black';
+}
+function changeThirdItem(){
+    document.getElementById('border-bottom-third').style.borderBottom = '0px solid black';
+}
+function changeFourthItem(){
+    document.getElementById('border-bottom-fourth').style.borderBottom = '0px solid black';
+}
+/* border-bottom returns */
+function rechangeFirstItem() {
+    document.getElementById('border-bottom-first').style.borderBottom = '5px solid black';
+}
+function rechangeSecondItem() {
+    document.getElementById('border-bottom-second').style.borderBottom = '5px solid black';
+}
+function rechangeThirdItem() {
+    document.getElementById('border-bottom-third').style.borderBottom = '5px solid black';
+}
+function rechangeFourthItem() {
+    document.getElementById('border-bottom-fourth').style.borderBottom = '5px solid black';
+}
