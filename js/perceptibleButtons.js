f8362980ed9e045e63f14ff6c0a0a22a97cc08ae
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-11-01 18:00:05 +0300

    Update perceptibleButtons.js

--- js/perceptibleButtons.js
+++ js/perceptibleButtons.js
@@ -31,19 +31,19 @@ function perceptibleThirdButton() {
     document.getElementById('secondlink').style.color = 'rgb(60, 60, 60)';
     document.getElementById('secondlink').style.borderRadius = '10px';
 
-    document.getElementById('firstlink').style.textIndent = '-5px';
+    document.getElementById('firstlink').style.textIndent = '-10px';
     document.getElementById('firstlink').style.borderColor = 'rgb(40, 40, 40)';
     document.getElementById('firstlink').style.color = 'rgb(40, 40, 40)';
     document.getElementById('firstlink').style.borderRadius = '10px';
 }
 /* denying previous function */
 function simpleFirstButton() {
-    document.getElementById('secondlink').style.textIndent = '5px';
+    document.getElementById('secondlink').style.textIndent = '0px';
     document.getElementById('secondlink').style.borderColor = 'white';
     document.getElementById('secondlink').style.color = 'white';
     document.getElementById('secondlink').style.borderRadius = '0px'; 
 
-    document.getElementById('thirdlink').style.textIndent = '10px';
+    document.getElementById('thirdlink').style.textIndent = '0px';
     document.getElementById('thirdlink').style.borderColor = 'white';
     document.getElementById('thirdlink').style.color = 'white';
     document.getElementById('thirdlink').style.borderRadius = '0px'; 
@@ -51,12 +51,12 @@ function simpleFirstButton() {
 
 
 function simpleSecondButton() {
-    document.getElementById('firstlink').style.textIndent = '5px';
+    document.getElementById('firstlink').style.textIndent = '0px';
     document.getElementById('firstlink').style.borderColor = 'white';
     document.getElementById('firstlink').style.color = 'white';
     document.getElementById('firstlink').style.borderRadius = '0px'; 
 
-    document.getElementById('thirdlink').style.textIndent = '5px';
+    document.getElementById('thirdlink').style.textIndent = '0px';
     document.getElementById('thirdlink').style.borderColor = 'white';
     document.getElementById('thirdlink').style.color = 'white';
     document.getElementById('thirdlink').style.borderRadius = '0px'; 
@@ -64,12 +64,12 @@ function simpleSecondButton() {
 
 
 function simpleThirdButton() {
-    document.getElementById('secondlink').style.textIndent = '5px';
+    document.getElementById('secondlink').style.textIndent = '0px';
     document.getElementById('secondlink').style.borderColor = 'white';
     document.getElementById('secondlink').style.color = 'white';
     document.getElementById('secondlink').style.borderRadius = '0px'; 
 
-    document.getElementById('firstlink').style.textIndent = '10px';
+    document.getElementById('firstlink').style.textIndent = '0px';
     document.getElementById('firstlink').style.borderColor = 'white';
     document.getElementById('firstlink').style.color = 'white';
     document.getElementById('firstlink').style.borderRadius = '0px'; 
